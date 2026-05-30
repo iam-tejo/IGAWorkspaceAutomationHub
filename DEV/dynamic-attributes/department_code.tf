91958ef71ddbe5943bbb08b9c5483871e4f445c1
Envizor GitOps Engine <gitops@envizor.io>
2026-05-30 16:21:52 +0100

    Envizor GitOps: Automatically synchronized workspace structure and baseline configs

--- DEV/dynamic-attributes/department_code.tf
+++
@@ -1,6 +0,0 @@
-resource "saviynt_dynamic_attribute" "department_code" {
-  attribute_id = "da-dev-1"
-  name         = "department_code"
-  value        = "HR-DEV"
-  description  = "Department code dynamic attribute"
-}
--- DEV/lookups/deptmappings.tf
+++
@@ -1,6 +0,0 @@
-resource "saviynt_lookup" "DeptMappings" {
-  lookup_id   = "lk-dev-2"
-  name        = "DeptMappings"
-  value       = "HR=100,FIN=200,ENG=300"
-  description = "Department mapping codes"
-}
