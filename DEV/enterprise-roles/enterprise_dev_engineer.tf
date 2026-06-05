5e05da3653cc869b4265ac4570e9b8bfc7ab675c
Envizor GitOps Engine <gitops@envizor.io>
2026-06-05 10:25:12 +0100

    Envizor GitOps: Automatically synchronized workspace structure and baseline configs

---
+++ DEV/enterprise-roles/enterprise_dev_engineer.tf
@@ -0,0 +1,5 @@
+resource "saviynt_enterprise_role" "Enterprise_Dev_Engineer" {
+  role_id     = "er-dev-1"
+  name        = "Enterprise_Dev_Engineer"
+  description = "General Enterprise Role for dev engineers"
+}
---
+++ DEV/security-systems/hr_system_dev.tf
@@ -0,0 +1,5 @@
+resource "saviynt_security_system" "HR_SYSTEM_DEV" {
+  system_id   = "ss-dev-1"
+  name        = "HR_SYSTEM_DEV"
+  description = "HR Security System for Development"
+}
