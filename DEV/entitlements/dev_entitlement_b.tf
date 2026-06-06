12c9f85fc27425353e3a9698fc41a207ea9d3922
iam-tejo <vamsi.idm@gmail.com>
2026-06-06 22:02:32 +0100

    Delete DEV/entitlements/dev_entitlement_b.tf

--- DEV/entitlements/dev_entitlement_b.tf
+++
@@ -1,6 +0,0 @@
-resource "saviynt_entitlement" "Dev Entitlement B" {
-  entitlement_id    = "ent-dev-2"
-  name              = "Dev Entitlement B"
-  entitlement_value = "read-write"
-  description       = "Read-write access entitlement"
-}
