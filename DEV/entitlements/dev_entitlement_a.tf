07adafc1ee5b14935b5fec9808943f668bf51dfe
iam-tejo <vamsi.idm@gmail.com>
2026-06-06 23:10:20 +0100

    Delete DEV/entitlements/dev_entitlement_a.tf

--- DEV/entitlements/dev_entitlement_a.tf
+++
@@ -1,6 +0,0 @@
-resource "saviynt_entitlement" "Dev Entitlement A" {
-  entitlement_id    = "ent-dev-1"
-  name              = "Dev Entitlement A"
-  entitlement_value = "read-only"
-  description       = "Read-only access entitlement"
-}
